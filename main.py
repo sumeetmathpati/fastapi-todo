1333796336e19c0f98321d5694bdc05271ba8b8c
Sumeet <sumeet221b@gmail.com>
2022-02-14 09:51:45 +0530

    Added python dict as temporary db.

--- main.py
+++ main.py
@@ -2,6 +2,32 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+users = [
+    {
+        'id': 1,
+        'name': 'Sumeet'
+    },
+    {
+        'id': 2,
+        'name': 'Sumeet2'
+    }
+]
+
+notes = [
+    {
+        'user': 1,
+        'todo': 'To go shopping.'
+    },
+    {
+        'user': 2,
+        'todo': 'To go library.'
+    },
+    {
+        'user': 1,
+        'todo': 'Complete the project.'
+    }
+]
+
 @app.get("/")
 async def root():
     return {"message": "Hello world"}
